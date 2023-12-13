1059586475e66d41c0183e8de69f2ae840cbdb98
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
-  // your code here
+  if (typeof s !== 'string' || s.length === 0) {
+    return s; // Return the entire string for empty or non-string inputs
+  }
+
+  // Trim leading and trailing spaces
+  s = s.trim();
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  if (spaceIndex === -1) {
+    return s; // Return the entire string if there's no space
+  }
+
+  // Extract and return the first word
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
